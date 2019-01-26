1126c07fb12fb7aadf087d93a3d5444613566ad2
Martyn Eggleton <martyn.eggleton@gmail.com>
2019-01-26 05:47:41 +0000

    ?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='rockstarfoxdot',    # This is the name of your PyPI-package.
-   version='0.1',                          # Update the version number for new releases
+   version='0.101',                          # Update the version number for new releases
    url="https://github.com/stretchyboy/rockstarfoxdot",
    author="Martyn Eggleton",
    author_email="martyn.eggleton@gmail.com",
